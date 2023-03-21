a2cfa88b72954c3a7ab758020495a7815a9d80a3
Herlander <herlander97dasilva@gmail.com>
2023-03-21 09:46:24 -0300

    Spring Boot projet ToDo

---
+++ src/test/java/com/webMvc/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webMvc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
